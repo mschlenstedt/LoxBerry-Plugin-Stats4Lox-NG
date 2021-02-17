8a28ef5ee0bec9faca92e3cbcd164676467aa313
MIchael Schlenstedt <Michael@schlenn.net>
2021-02-17 14:14:44 +0100

    Move InfluxDB config to loxberry

--- postroot.sh
+++ postroot.sh
@@ -20,40 +20,69 @@ PCONFIG=$LBPCONFIG/$PDIR
 PSBIN=$LBPSBIN/$PDIR
 PBIN=$LBPBIN/$PDIR
 
-# Srating InfluxDB
-if [ ! -x /usr/bin/influxd ]; then
+INFLUXDBIN=`which influxd`
+INFLUXBIN=`which influx`
+OPENSSLBIN=`which openssl`
+
+# Checking for InfluxDB
+if [ ! -x $INFLUXDBIN ]; then
 	echo "<FAIL> Seems that InfluxDB was not installed correctly. Giving up."
 	exit 2
 fi
 
-echo "Starting InfluxDB..."
+echo "<INFO> Starting InfluxDB..."
 systemctl unmask influxdb.service
 systemctl enable --now influxdb
 
 service influxdb status
 if [ $? -ne 0 ]; then
-	echo "<ERROR> Seems that InfluxDB could nnot be started. Nevertheless, I will try to continue."
-	exit 1
+	echo "<FAIL> Seems that InfluxDB could not be started. Giving up."
+	exit 2
+else
+	echo "<OK> InfluxDB service is running."
 fi
 
-echo "Current configuration of InfluxDB is as follows:"
-influxd config
+echo "<INFO> Current configuration of InfluxDB is as follows:"
+$INFLUXDBIN config
 
-echo "Creating default InfluxDB user loxberry/loxberry."
-influx -execute "CREATE USER loxberry WITH PASSWORD 'loxberry' WITH ALL PRIVILEGES"
-if [ $? -ne 0 ]; then
-	echo "<ERROR> Could not create default InfluxDB user. Nevertheless, I will try to continue."
-	exit 1
+USEREXISTS=0
+RESP=`$INFLUXBIN -execute "SHOW USERS" | grep -e "^loxberry\W*true$" | wc -l`
+if [ $RESP -eq 0 ]; then
+	echo "<INFO> Creating default InfluxDB user loxberry/loxberry."
+	$INFLUXBIN -execute "CREATE USER loxberry WITH PASSWORD 'loxberry' WITH ALL PRIVILEGES"
+	if [ $? -ne 0 ]; then
+		echo "<ERROR> Could not create default InfluxDB user. Nevertheless, I will try to continue. You have to make sure that a user 'loxberry' with password 'loxberry' exists!"
+		exit 1
+	else
+		echo "<OK> InfluxDB user loxberry created sucessfully."
+	fi
+else
+	echo "<OK> InfluxDB user loxberry already exists. Note! Skippig also creation of new loxberry database!"
+	USEREXISTS=1
 fi
 
-echo "Creating defailt InfluxDB database loxberry."
-influx -username loxberry -password loxberry -execute "CREATE DATABASE loxberry"
-if [ $? -ne 0 ]; then
-	echo "<ERROR> Could not create default InfluxDB database. Nevertheless, I will try to continue."
-	exit 1
+
+if [ $USEREXISTS -eq 0 ]; then
+	echo "<INFO> Creating default InfluxDB database loxberry."
+	$INFLUXBIN -username loxberry -password loxberry -execute "CREATE DATABASE loxberry"
+	if [ $? -ne 0 ]; then
+		echo "<ERROR> Could not create default InfluxDB database. Nevertheless, I will try to continue. You have to make sure that a idatabase 'loxberry' exists!"
+		exit 1
+	fi
+	echo "<INFO> Current available InfluxDB databases are as follows:"
+	$INFLUXBIN -username loxberry -password loxberry -execute "SHOW DATABASES"
 fi
 
-echo "Current available InfluxDB databases are as follows:"
-influx -username loxberry -password loxberry -execute "SHOW DATABASES"
+echo "<INFO> Creating (new) self-signed SSL certificates."
+$OPENSSLBIN req -x509 -nodes -newkey rsa:2048 -keyout $PCONFIG/influxdb-selfsigned.key -out $PCONFIG/influxdb-selfsigned.crt -days 3650 -subj "/C=DE/ST=Saxony/L=Dresden/O=LoxBerry"
+chown influxdb:influxdb $PCONFIG/influxdb-selfsigned.*
+
+echo "<INFO> Activating new InfluxDB configuration."
+if [ -d /etc/influxdb ] && [ ! -L /etc/influxdb ]; then
+	mv /etc/influxdb /etc/influxdb.orig
+fi
+rm -rf /etc/influxdb
+ln -s $PCONFIG /etc/influxdb
+systemctl restart influxdb
 
 exit 0
