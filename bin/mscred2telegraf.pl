1f5e14bd7b3cacfff86fe2011b6753617777ee24
mschlenstedt <Michael@schlenn.net>
2021-02-24 05:36:07 +0100

    Clean exit

--- bin/mscred2telegraf.pl
+++ bin/mscred2telegraf.pl
@@ -17,3 +17,5 @@ foreach my $ms (sort keys %miniservers) {
 	system "awk -v s=\"USER_MS$ms=\\\"$miniservers{$ms}{Admin_RAW}\\\"\" '/^USER_MS$ms=/{\$0=s;f=1} {a[++n]=\$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' $lbpconfigdir/telegraf/telegraf.env";
 	system "awk -v s=\"PASS_MS$ms=\\\"$miniservers{$ms}{Pass_RAW}\\\"\" '/^PASS_MS$ms=/{\$0=s;f=1} {a[++n]=\$0} END{if(!f)a[++n]=s;for(i=1;i<=n;i++)print a[i]>ARGV[1]}' $lbpconfigdir/telegraf/telegraf.env";
 }
+
+exit(0);
