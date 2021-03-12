94db826a7b5baa79f5fce0741af184a49bcbed16
Christian Fenzl <fenzl@t-r-t.at>
2021-03-13 00:26:44 +0100

    Block names in different languages

---
+++ bin/create_element_langfiles.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use LoxBerry::System;
+use File::Basename;
+use XML::LibXML;
+
+our $src_directory = '/tmp';
+
+get_sourcefiles();
+
+sub get_sourcefiles
+{
+	my @files = glob( $src_directory . '/HelpDesc_*.LxRes' );
+	
+	if( ! @files ) {
+		print 'Copy C:\Program Files (x86)\Loxone\LoxoneConfig\HelpDesc_*.LxRes to /tmp'."\n";
+		print "The script directly creates language files in\n";
+		print "$lbptemplatedir/lang/ as loxelements*.ini\n";
+		exit;
+	}
+	
+	foreach my $fullpath (@files) {
+		my ($file, $dirs) = File::Basename::fileparse($fullpath);
+		my ($filename, $fileext) = split(/\./, $file);
+		my ($prefix, $lang) = split( "_", $filename );
+		
+		my $langfilename = $lbptemplatedir.'/lang/loxelements_'.lc(substr($lang, 0, 2)).'.ini';
+		
+		
+		print "$file: Lang $lang\n";
+		my $destfile = File::Basename::dirname($fullpath).'/'.$filename.'.xml';
+		unpack_file( $fullpath, $destfile);
+		my $elements;
+		if( ! -e $destfile ) {
+			print "Could not find $destfile for parsing.\n";
+			next;
+		}
+		$elements = parse_xml( $destfile );
+		if( ! $elements ) {
+			print "Could not parse XML of $destfile.\n";
+			next;
+		}
+		
+		# Create language file
+		my $ini = "[ELEMENTS]\n";
+		foreach( sort keys %$elements ) {
+			$ini .= $_.'="'.$elements->{$_}.'"'."\n";
+		}
+		eval {
+			open(my $fh, '>', $langfilename);
+			print $fh $ini;
+			close $fh;
+		};
+		if( $@ ) {
+			print "Error writing file $langfilename: $!\n";
+		}
+		else {
+			print "File $langfilename written\n";
+		}
+	}
+}
+
+
+
+
+
+sub unpack_file {
+	my ($inputfile, $outputfile) = @_;
+	
+	`${LoxBerry::System::lbpbindir}/libs/Loxone/unpack_loxcc.py "$inputfile" "$outputfile"`;
+}
+
+
+
+sub parse_xml {
+	
+	my ($inputfile) = @_;
+
+	my $xmlstr = LoxBerry::System::read_file( $inputfile );
+
+	# Loxone uses a BOM, that cannot be handled by the XML Parser
+	my $UTF8_BOM = chr(0xef) . chr(0xbb) . chr(0xbf);
+	if(substr( $xmlstr, 0, 3) eq $UTF8_BOM) {
+		$xmlstr = substr $xmlstr, 3;
+	}
+	$xmlstr = Encode::encode("utf8", $xmlstr);
+	
+	my $dom = XML::LibXML->load_xml(
+		string => $xmlstr
+	);
+
+	my %elements;
+
+	my @nodes = $dom->findnodes('//String[@Type="NA"]');
+	
+	foreach my $element ( @nodes ) {
+		my $id = $element->{ID};
+		my $locatename = $element->{Text};
+		
+		my (undef, $name, undef) = split( "_", $id );
+		
+		$elements{$name} = $locatename;
+	}
+	
+	return \%elements;
+
+}
