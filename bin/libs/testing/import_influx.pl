0132cca5465fb9a33ee89d6a43cf84a956770053
Christian Fenzl <fenzl@t-r-t.at>
2021-04-03 17:20:51 +0200

    import: Add statistics and runtime estimations

--- bin/libs/testing/import_influx.pl
+++ bin/libs/testing/import_influx.pl
@@ -5,7 +5,7 @@ use LoxBerry::System;
 use LoxBerry::Log;
 use LoxBerry::JSON;
 use CGI;
-use Time::HiRes;
+use Time::HiRes qw(time);
 use FindBin qw($Bin);
 use lib "$Bin/..";
 use Loxone::Import;
@@ -80,7 +80,10 @@ if( ! $#statmonths ) {
 	exit(2);
 }
 
-
+my $months_count_full = scalar @statmonths;
+my $months_count_finished = 0;
+my $record_count = 0;
+my $duration_time_secs = 0;
 # print Data::Dumper::Dumper( $import->{statlistAll} );
 
 foreach my $yearmonth ( @statmonths ) {
@@ -104,7 +107,40 @@ foreach my $yearmonth ( @statmonths ) {
 	);	
 	
 	$status->{finished}{$yearmonth} = \%finished;
-	supdate( { } );  
+	
+	# Calculate runtime estimations 
+	$record_count += $fullcount;
+	$months_count_finished++;
+	$duration_time_secs += $finished{duration};
+	my $avg_records_per_month = ($record_count/$months_count_finished);
+	my $avg_time_per_record_secs = $duration_time_secs / $record_count;
+	my $estimate_records_left =  $avg_records_per_month * ($months_count_full-$months_count_finished);
+	my $estimate_time_left_secs = $estimate_records_left*$avg_time_per_record_secs;
+	my $estimate_time_left_time_obj = time()+$estimate_time_left_secs;
+	my ($e_sec,$e_min,$e_hour,$e_mday,$e_mon,$e_year,$e_wday,$e_yday,$e_isdst) =
+                                            localtime(time()+$estimate_time_left_secs);
+	$e_year+=2000;
+	my $estimate_time_left_time_hr = sprintf("%04d-%02d-%02d %02d:%02d:%02d", $e_year, $e_mon,$e_mday, $e_hour, $e_min, $e_sec);
+	
+	# my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+                                            localtime(time);
+	# my $timenow = "$year-$mon-$mday $hour:$min:$sec";
+	
+	my %stats = (
+		months_count_full => $months_count_full,
+		months_count_finished => $months_count_finished,
+		record_count_finished => $record_count,
+		duration_time_secs => $duration_time_secs,
+		avg_records_per_month => $avg_records_per_month,
+		avg_time_per_record_secs => $avg_time_per_record_secs,
+		estimate_records_left => $estimate_records_left,
+		estimate_time_left_secs => $estimate_time_left_secs,
+		estimate_time_left_time_hr => $estimate_time_left_time_hr,
+	#	timenow => $timenow
+	);
+		
+	
+	supdate( { stats => \%stats } );  
 
 	
 }
@@ -120,7 +156,10 @@ sub END {
 			supdate( { status => "error" } );
 			LOGCRIT "Import exited with error.";
 		}
-		supdate( { endtime => time() } );
+		supdate( { 
+			endtime => time(), 
+			duration =>  int((time()-$status->{starttime})*1000)/1000
+		} );
 	}
 	eval { 
 		LOGEND;
