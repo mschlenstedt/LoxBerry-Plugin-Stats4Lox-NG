09908e8ba30b7db8cd821998fdf2b4ce83e744a4
Christian Fenzl <fenzl@t-r-t.at>
2021-05-10 15:36:13 +0200

    Update loxone_sub_navbar.js
    - Fixed link to main_loxone.cgi

--- webfrontend/htmlauth/js/loxone_sub_navbar.js
+++ webfrontend/htmlauth/js/loxone_sub_navbar.js
@@ -4,7 +4,7 @@ $(function() {
 	navbarHtml += `
 	<div data-role="navbar" class="ui-navbar" role="navigation" id="s4l_sub_nav">
 		<ul class="ui-grid-d">
-			<li class="ui-block-a"><a href="index.cgi" id="submenu1" class="ui-link ui-btn">Statistic Selection</a></li>
+			<li class="ui-block-a"><a href="main_loxone.cgi" id="submenu1" class="ui-link ui-btn">Statistic Selection</a></li>
 			<li class="ui-block-b"><a href="loxone_import_report.cgi" id="submenu2" class="ui-link ui-btn">Import Report</a></li>
 			<li class="ui-block-c"><a href="mqttlive_loxone.cgi" id="submenu3" class="ui-link ui-btn">MQTT Live</a></li>
 		</ul>
