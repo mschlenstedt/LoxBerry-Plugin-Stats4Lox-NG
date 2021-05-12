cdb97ec820029165bcdd32374f1de984442c350d
Christian Fenzl <fenzl@t-r-t.at>
2021-05-12 12:42:39 +0200

    testtimestamps.pl: Testing tool for Loxone Statistic timestamps
    Regarding to #53

--- bin/libs/testing/testtimestamps.pl
+++ bin/libs/testing/testtimestamps.pl
@@ -1,19 +1,26 @@
 #!/usr/bin/perl
+use warnings;
+use strict;
 use DateTime;
 our $LocalTZ = DateTime::TimeZone->new( name => 'local' );
 
+if( !$ARGV[1] ) {
+	print "Checks a timestamp of a Loxone Statistic against the Import routine that does epoch and\ntimezone/daylight saving conversion.\n";
+	
+	print "Calling Syntax:\n";
+	print "  $0 <date> <time> (without quotes)\n";
+	print "e.g.\n";
+	print "  $0 2021-03-08 10:56:14\n";
+	exit(1);
+}
 
-$timestr1="2000-03-01 10:00:00"; 
-$timestr2="2000-05-01 10:00:00"; 
+my $timestr1 = "$ARGV[0] $ARGV[1]";
 
 my $time1 = createDateTime( $timestr1 );
-my $time2 = createDateTime( $timestr2 );
 
 
 print $time1->hires_epoch()."\n";
-print $time2->hires_epoch()."\n";
 print $time1->hms."\n";
-print $time2->hms."\n";
 
 
 sub createDateTime
