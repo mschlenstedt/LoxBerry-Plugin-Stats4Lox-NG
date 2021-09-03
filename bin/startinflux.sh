b66c1436f0a87bce0900847c281ca2b60444b3d5
Christian Fenzl <fenzl@t-r-t.at>
2021-09-03 15:37:10 +0200

    Replace original influx start script by S4L.own
    - Original start script tests Influx http connection for 10 seconds and dies. Systemd then will restart the service.
    As Influx on Pi is not fast enough to bring up it's http interface within 10 seconds, systemd will create a 10-second-restart-loop.
    
    This commit:
    - Includes an own start script (without http probe)
    - Override the ExecStart of the systemd service to use our file for Influx.

---
+++ bin/startinflux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+echo Starting Influx via S4L starting script
+/usr/bin/influxd -config /etc/influxdb/influxdb.conf $INFLUXD_OPTS &
+PID=$!
+echo $PID > /var/lib/influxdb/influxd.pid
+
